ef2bc946a2f7c23fd0bb2670f4381dec340989db
antonbormotov <qqwrst@gmail.com>
2016-11-06 17:29:35 +0800

    Added title to table's columns

--- index.php
+++ index.php
@@ -7,20 +7,26 @@ $pdo = db_connect($config);
 $results = get_data($pdo);
 
 echo '<table>';
+echo '<tr>';
+echo '<td>Time</td>';
+echo '<td>Cpu, %</td>';
+echo '<td>Used memory, %</td>';
+echo '<td>Hdd, %</td>';
+echo '</tr>';
 
 foreach ($results as $result) {
     echo '<tr>';
     echo '<td>';
-    echo $result['time'];
+    echo $result['time'] . '|';
     echo '</td>';
     echo '<td>';
-    echo $result['cpu'];
+    echo $result['cpu'] . '|';
     echo '</td>';
     echo '<td>';
-    echo $result['memory'];
+    echo $result['memory'] . '|';
     echo '</td>';
     echo '<td>';
-    echo $result['hdd'];
+    echo $result['hdd'] . '|';
     echo '</td>';
     echo '</tr>';
 }
