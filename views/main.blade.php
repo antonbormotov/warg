0276ac3e8b0daad514f28b07e44926f1278c050e
antonbormotov <qqwrst@gmail.com>
2016-11-12 16:32:58 +0800

    Added composer, trying blade templates

---
+++ views/main.blade.php
@@ -0,0 +1,17 @@
+<?php /*use iprice\frontend\Util;*/ ?>
+
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+</head>
+<body>
+    <table>
+    <tr>
+        <td colspan="{{{ count($results) }}}">
+            Load by time
+        </td>
+    </tr>
+
+    </table>
+</body>
+</html>
